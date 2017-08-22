c401bc40e18ba0b906a445520d6e02b81deeda22
I. Saber <isaber23@outlook.com>
2017-08-22 20:41:04 +0100

    added react portfolio widget

---
+++ js/assets/data.js
@@ -0,0 +1,99 @@
+
+
+_tags = ['all','javascript', 'react','html','jquery','es6', 'css', 'css3','html5','bootstrap'];
+_projects = [
+    {
+        name:'Weather App', 
+        image_url:'http://i.imgur.com/vli1EW8.png',
+        github_url:'https://github.com/icybears/weatherApp',
+        app_url:'https://si-weather-app.firebaseapp.com/',
+        tags:['all','javascript','react','es6','css3','css','html','html5'],
+        codepen_url: null
+    },
+    {
+        name:'TO-DO App',
+        image_url:'http://i.imgur.com/XrJa1ev.png',
+        github_url:'https://github.com/icybears/TODO-App',
+        app_url:'https://si-todo-app.firebaseapp.com/',
+        tags:['all','javascript','react','es6','css3','css','html','html5'],
+        codepen_url: null
+    },
+     {
+        name:'freeCodeCamp Leaderboard',
+        image_url:'http://i.imgur.com/byrq5IN.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','javascript','react','es6','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/RZPgoe/'
+    },
+    {
+        name:'Wikipedia Viewer',
+        image_url:'http://i.imgur.com/OmDukgs.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','javascript','jquery','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/dWVMVN/'
+    },
+     {
+        name:'Calculator',
+        image_url:'http://i.imgur.com/wNFq9vE.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','javascript','jquery','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/QvYGqM/'
+    },
+     {
+        name:'Tic Tac Toe',
+        image_url:'http://i.imgur.com/7aJbA0V.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','javascript','jquery','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/aWrpGM/'
+    },
+    {
+        name:'Quote Generator',
+        image_url:'http://i.imgur.com/AHqH7CK.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','javascript','jquery','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/mmmmdK/'
+    },
+       {
+        name:'Simon Game',
+        image_url:'http://i.imgur.com/cAbsjL8.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','javascript','jquery','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/RgGxdp/'
+    },
+    {
+        name:'Pomodoro Clock',
+        image_url:'http://i.imgur.com/4dLwIxP.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','javascript','jquery','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/WjWJBV/'
+    },
+     {
+        name:'Responsive Design 2',
+        image_url:'http://i.imgur.com/eI28ery.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','css3','css','html','html5','bootstrap'],
+        codepen_url: 'https://codepen.io/icybears/full/LxQeBv/'
+    },
+        {
+        name:'Responsive Design ',
+        image_url:'http://i.imgur.com/UmzNY7H.png',
+        github_url:null,
+        app_url:null,
+        tags:['all','css3','css','html','html5'],
+        codepen_url: 'https://codepen.io/icybears/full/zNBLzE/'
+    },
+    
+    
+    
+    
+    
+    
+];
